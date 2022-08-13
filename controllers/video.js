f3fbe81b88246f8ae3fb65010b2b3f7cc057a5c7
Venkat <venkatramanbehera1@gmail.com>
2022-08-13 20:25:16 +0530

    project updated

--- controllers/video.js
+++ controllers/video.js
@@ -93,7 +93,6 @@ export const subs = async (req, res, next) => {
 };
 export const getByTag = async (req, res, next) => {
   const tags = req.query.tags.split(",");
-  // console.log(tags);
   try {
     const videos = await Video.find({ tags: { $in: tags } }).limit(20);
     res.status(200).json(videos);
